8f24f04e3db049e1029b9bb72e316246cde11806
Temirlan Ilik <0penguin@mail.ru>
2025-02-26 13:20:43 +0500

    step-6-add-people-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,37 +1,35 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-import Header from '../header'
-import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import PersonDetails from "../person-details";
 
-import './app.css'
+import "./app.css";
+import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
-export default class App extends Component{
+export default class App extends Component {
+  state = {
+    selectedPerson: null,
+    hasError: false,
+  };
 
-    state = {
-        selectedPerson: null
+  componentDidCatch(error, errorInfo) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />;
     }
 
-    onPersonSelected = (id) => {
-        this.setState({ selectedPerson: id })
-    }
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                <RandomPlanet/>
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={ this.onPersonSelected }/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={ this.state.selectedPerson }/>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+        <PeoplePage />
+      </div>
+    );
+  }
 }
