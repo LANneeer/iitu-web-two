2b354d5f35b7c80717dbfbc512600aae5aa828fe
Temirlan Ilik <0penguin@mail.ru>
2025-03-26 09:40:04 +0500

    step-18-props-to-compose

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -5,80 +5,80 @@ import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner";
 
 export default class PersonDetails extends Component {
-  swapiService = new SwapiService();
+	swapiService = new SwapiService();
 
-  state = {
-    person: null,
-    loading: true,
-  };
+	state = {
+		person: null,
+		loading: true,
+	};
 
-  componentDidMount() {
-    this.updatePerson();
-  }
+	componentDidMount() {
+		this.updatePerson();
+	}
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.personId !== prevProps.personId) {
-      this.updatePerson();
-      this.setState({ loading: true });
-    }
-  }
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		if (this.props.personId !== prevProps.personId) {
+			this.updatePerson();
+			this.setState({ loading: true });
+		}
+	}
 
-  updatePerson = () => {
-    const { personId } = this.props;
-    if (!personId) {
-      return;
-    }
+	updatePerson = () => {
+		const { personId } = this.props;
+		if (!personId) {
+			return;
+		}
 
-    this.swapiService.getPerson(personId).then((person) => {
-      this.setState({ person, loading: false });
-    });
-  };
+		this.swapiService.getPerson(personId).then((person) => {
+			this.setState({ person, loading: false });
+		});
+	};
 
-  render() {
-    if (!this.state.person) return <span>Select a person from a list</span>;
+	render() {
+		if (!this.state.person) return <span>Select a person from a list</span>;
 
-    const { person, loading } = this.state;
+		const { person, loading } = this.state;
 
-    const spinner = loading ? <Spinner /> : null;
+		const spinner = loading ? <Spinner /> : null;
 
-    const content = !loading ? <PersonDetailsView person={person} /> : null;
-    return (
-      <div className="person-details card">
-        {spinner}
-        {content}
-      </div>
-    );
-  }
+		const content = !loading ? <PersonDetailsView person={person} /> : null;
+		return (
+			<div className="person-details card">
+				{spinner}
+				{content}
+			</div>
+		);
+	}
 }
 
 const PersonDetailsView = ({ person }) => {
-  const { id, name, gender, birthYear, eyeColor } = person;
+	const { id, name, gender, birthYear, eyeColor } = person;
 
-  return (
-    <React.Fragment>
-      <image
-        className="person-image"
-        alt="person image"
-        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-      />
+	return (
+		<React.Fragment>
+			<image
+				className="person-image"
+				alt="person image"
+				src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+			/>
 
-      <div className="card-body">
-        <h4>{name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <span className="term">Gender:</span>
-            <span>{gender}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Birth Year:</span>
-            <span>{birthYear}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Eye Color:</span>
-            <span>{eyeColor}</span>
-          </li>
-        </ul>
-      </div>
-    </React.Fragment>
-  );
+			<div className="card-body">
+				<h4>{name}</h4>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<span className="term">Gender:</span>
+						<span>{gender}</span>
+					</li>
+					<li className="list-group-item">
+						<span className="term">Birth Year:</span>
+						<span>{birthYear}</span>
+					</li>
+					<li className="list-group-item">
+						<span className="term">Eye Color:</span>
+						<span>{eyeColor}</span>
+					</li>
+				</ul>
+			</div>
+		</React.Fragment>
+	);
 };
